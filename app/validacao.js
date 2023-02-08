4e1ee9fa586e3e37ccfcbe8d6b52a6ee5abc28bb
Gabriel Assys <brachak2001@gmail.com>
2023-02-08 11:08:46 -0300

    adicionando JavaScript

---
+++ app/validacao.js
@@ -0,0 +1,50 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += `
+            <div>Valor inválido</div>
+        `
+        return
+    }
+
+    if (chuteMaiorOuMenorQuePermitido(numero)) {
+        elementoChute.innerHTML += `
+            <div>
+                Valor inváldo: o número secreto precisa estar entre ${menorValor} e ${maiorValor}
+            </div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-down"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-up"></i></div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function chuteMaiorOuMenorQuePermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e =>{
+    if(e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
